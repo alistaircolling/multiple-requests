2ac1d587f655cfb36ebae124e67b8f8687a28f2d
Alistair Colling <alistair@Alistairs-MacBook-Pro.local>
2020-08-02 12:32:32 +0100

    test working for async

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 const fetch = require("node-fetch");
 
-const requestMutliple = async (urls) => {
-  const promises = urls.map((url) => {
-    return fetch(url);
-  });
-  console.log(promises);
-
-  return Promise.allSettled(promises)
-    .then((response) => {
-      console.log(response);
-      response.forEach((respons) => console.log(respons));
-      return response;
-    })
-    .catch((err) => console.log(err));
+const requestPromises = (promises) => {
+  const promise = promises.shift();
+  promise.then;
 };
 
+const requestMutliple = async (urls) => {
+  const result = await fetch(urls[0]).then((result) => result.text());
+  return result;
+};
+//   const promises = urls.map((url) => {
+//     return fetch(url);
+//   });
+//   //Promise.allSettled(promises).then((result) => console.log(result));
+
+//   // return promises
+//   //   .reduce((promiseChain, currentTask) => {
+//   //     return promiseChain.then((chainResults) =>
+//   //       currentTask.then((currentResult) => {
+//   //         console.log("current", currentResult.json());
+//   //         return [...chainResults, currentResult];
+//   //       })
+//   //     );
+//   //   }, Promise.resolve([]))
+//   //   .then((arrayOfResults) => {
+//   //     // Do something with all results
+//   //     arrayOfResults.map((result) => console.log(result.));
+//   //   });
+
+//   return Promise.all(promises)
+//     .then((response) => {
+//       const results = [];
+//       response.map((item) =>
+//         item
+//           .text()
+//           .then((parsed) => results.push(parsed))
+//           .then((results) => {
+//             console.log(results);
+//             return Promise.resolve(results);
+//           })
+//       );
+//     })
+//     .catch((err) => console.log(err));
+// };
+
 module.exports = requestMutliple;
+
+// return Promise.all(promises)
+//   .then((response) => {
+//     return response[0].text();
+//   })
+//   .then((parsed) => console.log(parsed))
+//   .catch((err) => console.log(err));
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,12 +1,17 @@
 const requestMutliple = require("../index");
+// var fs = require("fs");
+
+// fs.readFile("temp.txt", function(err, buf) {
+//   console.log(buf.toString());
+// });
 
 const urls = [
   "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/ftse-fsi.json",
+  "https://ft-tech-test-example.s3-eu-west-1.amazonaws.com/ftse-fsi.json",
 ];
 
-test("hello", () => {
-  return requestMutliple(urls).then((data) => {
-    console.log(data);
-    expect(data).toBe(3);
-  });
+test("hello", async () => {
+  const result = await requestMutliple(urls);
+  console.log(result);
+  expect(result).toBe(2);
 });
